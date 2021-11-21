8458a51e68abec1078d32dfe8e6819cca7d4d284
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-11-21 18:33:17 +0530

    fixed navbar active link in side bar

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -2,24 +2,24 @@
     <div class="pcoded-inner-navbar main-menu">
         <div class="pcoded-navigatio-lavel">Navigation</div>
         <ul class="pcoded-item pcoded-left-item">
-            <li class="active">
+            <li class="{{ Route::is('dashboard') ? 'active' : ' ' }}">
                 <a href="{{ route('dashboard') }}" class="active">
                     <span class="pcoded-micon"><i class="feather icon-home"></i></span>
                     <span class="pcoded-mtext">Dashboard</span>
                 </a>
             </li>
-            <li class="pcoded-hasmenu">
+            <li class="pcoded-hasmenu {{ Route::is('admin.products.add')||Route::is('admin.products.all') ? 'active pcoded-trigger' : ' ' }} ">
                 <a href="javascript:void(0)">
                     <span class="pcoded-micon"><i class="feather icon-shopping-cart"></i></span>
                     <span class="pcoded-mtext">Products</span>
                 </a>
                 <ul class="pcoded-submenu">
-                    <li class="">
+                    <li class=" {{ Route::is('admin.products.add') ? 'active' : ' ' }}">
                         <a href="{{ route('admin.products.add') }}">
                             <span class="pcoded-mtext">Add Product</span>
                         </a>
                     </li>
-                    <li class="">
+                    <li class=" {{ Route::is('admin.products.all') ? 'active' : ' ' }}">
                         <a href="{{ route('admin.products.all') }}">
                             <span class="pcoded-mtext">All Products</span>
                         </a>
@@ -27,7 +27,8 @@
 
                 </ul>
             </li>
-            <li>
+            <!-- make link active on his route -->
+            <li class="{{ Route::is('admin.homeslider') ? 'active' : ' ' }}">
                 <a href="{{ route('admin.homeslider') }}">
                     <span class="pcoded-micon"><i class="feather icon-image"></i></span>
                     <span class="pcoded-mtext">Home Slider</span>
