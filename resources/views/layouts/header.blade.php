13b2fe626015fbb8d72675443cba16e319b86bc3
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-11-25 18:13:20 +0530

    Frontend theme setup complete

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,149 @@
+<header class="main-header main-header--one  clearfix">
+    <div class="main-header--one__wrapper">
+        <div class="main-header--one__top clearfix">
+            <div class="auto-container">
+
+                <div class="main-header--one__top-left">
+                    <div class="text">
+                        <p>Welcome to Agriculture Farming WordPress Theme</p>
+                    </div>
+                    <div class="social-link clearfix">
+                        <ul>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="main-header--one__top-right clearfix">
+                    <ul>
+                        <li>
+                            <div class="icon">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <div class="text">
+                                <p><a href="mailto:needhelp@company.com">needhelp@company.com</a></p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <i class="fa fa-clock"></i>
+                            </div>
+                            <div class="text">
+                                <p>Mon - Sat 8:00 - 6:30, Sunday - CLOSED</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-header--one__bottom clearfix">
+            <div class="auto-container">
+                <div class="main-header--one__bottom-inner">
+                    <nav class="main-menu main-menu--1">
+                        <div class="main-menu__inner">
+                            <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+
+                            <div class="stricky-one-logo">
+                                <div class="logo">
+                                    <a href="index.html"><img src="{{ asset('Frontend/assets/images/resources/logo-1.png'); }}"
+                                            alt=""></a>
+                                </div>
+                            </div>
+
+                            <div class="main-header--one__bottom-left">
+                                <ul class="main-menu__list">
+                                    <li class="dropdown current">
+                                        <a href="index.html">Home</a>
+                                        <ul>
+                                            <li><a href="index.html">Home One</a></li>
+                                            <li><a href="index-2.html">Home Two</a></li>
+                                            <li><a href="index-3.html">Home Three</a></li>
+                                            <li class="dropdown">
+                                                <a href="#">Header Styles</a>
+                                                <ul>
+                                                    <li><a href="index.html">Header One</a></li>
+                                                    <li><a href="index-2.html">Header Two</a></li>
+                                                    <li><a href="index-3.html">Header Three</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="about.html">About</a></li>
+                                    <li class="dropdown">
+                                        <a href="#">Services</a>
+                                        <ul>
+                                            <li><a href="services.html">All Services</a></li>
+                                            <li><a href="agriculture-products.html">Agriculture Products</a>
+                                            </li>
+                                            <li><a href="fresh-vegetable.html">Fresh Vegetable</a></li>
+                                            <li><a href="organic-products.html">Organic Products</a></li>
+                                            <li><a href="dairy-products.html">Dairy Products</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="#">Page</a>
+                                        <ul>
+                                            <li><a href="farmers.html">Farmers</a></li>
+                                            <li><a href="projects.html">Projects</a></li>
+                                            <li><a href="projects-details.html">Projects Details</a></li>
+                                            <li><a href="faq.html">Faq</a></li>
+                                            <li><a href="error.html">Error</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="shop.html">Shop</a>
+                                        <ul>
+                                            <li><a href="shop.html">Products</a></li>
+                                            <li><a href="product-details.html">Product Details</a></li>
+                                            <li><a href="cart.html">Cart</a></li>
+                                            <li><a href="checkout.html">Checkout</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="#">Blog</a>
+                                        <ul>
+                                            <li><a href="news.html">News</a></li>
+                                            <li><a href="news-v2.html">News Two</a></li>
+                                            <li><a href="news-details.html">News Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+
+                    <div class="main-header--one__bottom-middel">
+                        <div class="logo">
+                            <a href="index.html"><img src="{{ asset('Frontend/assets/images/resources/logo-1.png'); }}" alt="" /></a>
+                        </div>
+                    </div>
+
+                    <div class="main-header--one__bottom-right clearfix">
+                        <div class="search-cart">
+                            <a href="#" class="search search-toggler"><span
+                                    class="icon-magnifying-glass"></span></a>
+                            <a href="#" class="cart mini-cart__toggler"><span
+                                    class="icon-shopping-cart"></span></a>
+                        </div>
+                        <div class="contact-box">
+                            <div class="icon">
+                                <span class="icon-phone-call-2"></span>
+                            </div>
+                            <div class="text">
+                                <p>Call Anytime</p>
+                                <a href="tel:123456789">666 888 0000</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</header>
