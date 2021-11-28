55b389aba391c2c3f8c689d54f204ebf2ee62fd9
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-11-29 00:33:08 +0530

    backend: added visitlog

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,13 @@ use App\Models\Blog;
 use App\Models\homeslider;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Sarfraznawaz2005\VisitLog\Facades\VisitLog;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $visitlog = VisitLog::save();
         $data['sliders'] = homeslider::all();
         $data['products'] = Product::offset(0)->limit(2)->get();
         $data['blogs'] = Blog::offset(0)->limit(3)->get();
